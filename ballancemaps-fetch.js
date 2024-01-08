f80391cf31edf24ec287f21a1cfbd037f44f9566
Ghomist <ghomist@gmail.com>
2024-01-08 16:21:14 +0800

    update: ys new api

--- ballancemaps-fetch.js
+++ ballancemaps-fetch.js
@@ -1,7 +1,7 @@
 class MapDataFetcher {
   #username = "ballancemaps";
-  #indexLink = `http://cc.ysepan.com/f_ht/ajcx/ml.aspx?cz=ml_dq&_dlmc=${this.#username}&_dlmm=`;
-  #fileListLink = `http://cc.ysepan.com/f_ht/ajcx/wj.aspx?cz=dq&jsq=0&mlbh={index}&wjpx=1&_dlmc=${this.#username}&_dlmm=`;
+  #indexLink = `http://c6.ysepan.com/f_ht/ajcx/ml.aspx?cz=ml_dq&_dlmc=${this.#username}&_dlmm=`;
+  #fileListLink = `http://c6.ysepan.com/f_ht/ajcx/wj.aspx?cz=dq&jsq=0&mlbh={index}&wjpx=1&_dlmc=${this.#username}&_dlmm=`;
   #indexes = [];
   #mapList = {};
 
@@ -16,9 +16,11 @@ class MapDataFetcher {
     if (this.#indexes.length == 0) {
       const htmlString = await this.#getYsHtml(this.#indexLink);
       this.#indexes = Array.from(
-        htmlString.matchAll(new RegExp('<li[^<>]+id="ml_([0-9]+)"[^<>]*>.*?<a [^<>]*>([^<>]+)</a><label>([^<>]+)?</label>.*?</li>', "g"))
+        htmlString.matchAll(new RegExp(
+          '<li[^<>]+id="ml_([0-9]+)"[^<>]*>.*?<a [^<>]*>([^<>]+)</a><label>([^<>]+)?</label>.*?</li>', 'g'
+        ))
       ).map(matches => ({
-        id: matches[1], 
+        id: matches[1],
         name: matches[2],
         notes: (matches[3] ?? "")
       }));
@@ -68,11 +70,50 @@ class MapDataFetcher {
       method: 'GET',
       referrer: 'http://cg.ys168.com/f_ht/ajcx/000ht.html?bbh=1134',
     })
-    .then(response => response.text())
-    .then(text => text.substring(text.indexOf("<")))
-    .catch((error) => {
-      console.error('Error: ', error);
-    });
+      .then(response => response.text())
+      .then(text => {
+        const b64Part = text.substring(text.indexOf("]") + 1);
+
+        // deprecated method (escape)
+        // return decodeURIComponent(escape(atob(b64Part)));
+
+        return decodeURIComponent(
+          // non-standard impl of base64 converting
+          atob(b64Part)
+            .split("")
+            .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+            .join("")
+        );
+      })
+      .catch((error) => {
+        console.error('Error: ', error);
+      });
     return (responseString ?? "");
   };
 };
+
+function test() {
+  const fetcher = new MapDataFetcher();
+  fetcher.getGroupIndexes('Ballance自制地图').then(indexes => {
+    indexes.forEach((index) => {
+      fetcher.getMapList(index.id).then(maps => {
+        maps.forEach(map => {
+          console.log(JSON.stringify(map));
+        });
+      });
+    });
+  })
+}
+
+async function testAsync() {
+  const fetcher = new MapDataFetcher();
+  const indexes = await fetcher.getGroupIndexes('Ballance自制地图');
+  for (const index of indexes) {
+    const maps = await fetcher.getMapList(index.id);
+    for (const map of maps) {
+      console.log(JSON.stringify(map));
+    }
+  }
+}
+
+testAsync();
